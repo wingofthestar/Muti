38308eb8ef231171a917505e0acd71920cbf26db
wingofthestar <xueliuxing2011@163.com>
2017-08-13 10:00:55 +0800

    UpdateUserInfo采用动态SQL方式，并对所有可能出现的动态SQL拼接结果进行单元测试

--- src/main/java/site/yourdiary/dao/UserSpaceMapper.java
+++ src/main/java/site/yourdiary/dao/UserSpaceMapper.java
@@ -1,5 +1,6 @@
 package site.yourdiary.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 import site.yourdiary.domain.UserInfo;
 
@@ -8,5 +9,5 @@ public interface UserSpaceMapper {
 
     UserInfo getUserInfoByUserId(int userId);
 
-    int updateUserInfoByUserId(int userId);
+    int updateUserInfoByUserId(@Param("userId") int userId, @Param("userPhoto") String userPhoto, @Param("userIntroduce") String userIntroduce);
 }
--- src/test/java/site/yourdiary/dao/UserSpaceTest.java
+++ src/test/java/site/yourdiary/dao/UserSpaceTest.java
@@ -22,8 +22,18 @@ public class UserSpaceTest {
     private UserSpaceMapper userSpaceDao;
 
     @Test
-    public void testgetUserInfoByUserId(){
+    public void testGetUserInfoByUserId(){
        UserInfo userInfo = userSpaceDao.getUserInfoByUserId(1);
         System.out.println(userInfo);
+    }
+
+    @Test
+    public void testUpdateUserInfobyUserId(){
+        String photo = null;
+        //对动态SQL可能出现的所有情况进行测试
+//        int result = userSpaceDao.updateUserInfoByUserId(1,photo,"你好，世界");
+//        int result = userSpaceDao.updateUserInfoByUserId(1,"http://img.yourdiary.site/170429pic1.png","");
+        int result = userSpaceDao.updateUserInfoByUserId(1,"http://img.yourdiary.site/170429pic2.png","为了更美好的生活");
+        System.out.println(result);
     }
 }
